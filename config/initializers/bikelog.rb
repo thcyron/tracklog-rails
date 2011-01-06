9bc96cf112441af9b2b8ef078a43edbb6b31d652
Thomas Cyron <thomas@thcyron.de>
2011-01-06 22:17:15 +0100

    Implement optional HTTP authentication

---
+++ config/initializers/bikelog.rb
@@ -0,0 +1,11 @@
+module Bikelog
+  class Config
+    class << self
+      attr_accessor :password
+    end
+  end
+end
+
+# If not nil, a HTTP authentication will be performed at each
+# request.  Only the entered password is checked against this value.
+Bikelog::Config.password = "password"
