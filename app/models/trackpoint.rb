549ac89a83239648422365c00c96016bce1ae201
Thomas Cyron <thomas@thcyron.de>
2011-02-27 12:50:04 +0100

    Move haversine function into separate file

--- app/models/trackpoint.rb
+++ app/models/trackpoint.rb
@@ -1,8 +1,4 @@
-class Numeric
-  def to_rad
-    self * Math::PI / 180 
-  end
-end
+require "bikelog/haversine"
 
 class Trackpoint < ActiveRecord::Base
   belongs_to :track
@@ -13,7 +9,7 @@ class Trackpoint < ActiveRecord::Base
   validates :time,      :presence => true
 
   def distance_to_trackpoint(trackpoint)
-    haversine(self.latitude, trackpoint.latitude, self.longitude, trackpoint.longitude)
+    Bikelog.haversine(self.latitude, trackpoint.latitude, self.longitude, trackpoint.longitude)
   end
 
   def time_to_trackpoint(trackpoint)
@@ -27,16 +23,4 @@ class Trackpoint < ActiveRecord::Base
   def ascent_to_trackpoint(trackpoint)
     trackpoint.elevation - self.elevation
   end
-
-  def haversine(lat1, lat2, lon1, lon2)
-    r = 6371
-    dlat = (lat2 - lat1).to_rad
-    dlon = (lon2 - lon1).to_rad
-
-    a = Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.cos(lat1.to_rad) * Math.cos(lat2.to_rad) * Math.sin(dlon / 2) * Math.sin(dlon / 2)
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-    r * c * 1000
-  end
-  private :haversine
 end
---
+++ lib/bikelog/haversine.rb
@@ -0,0 +1,18 @@
+class Numeric
+  def to_rad
+    self * Math::PI / 180
+  end
+end
+
+module Bikelog
+  def self.haversine(lat1, lat2, lon1, lon2)
+    r = 6371
+    dlat = (lat2 - lat1).to_rad
+    dlon = (lon2 - lon1).to_rad
+
+    a = Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.cos(lat1.to_rad) * Math.cos(lat2.to_rad) * Math.sin(dlon / 2) * Math.sin(dlon / 2)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    r * c * 1000
+  end
+end
