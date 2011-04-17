21b47c8d7b0d9fb00d5aa7ebf29a807c941c2acc
Thomas Cyron <thomas@thcyron.de>
2011-04-17 13:47:52 +0200

    Put inline JS into a separate file

---
+++ public/javascripts/bikelog/site.js
@@ -0,0 +1,27 @@
+$(function() {
+  $("#track-rename-pane input[type='text']").keyup(function() {
+    var e = $("#track-name"),
+        name = $.trim($(this).val());
+
+    if (name.length > 0) {
+      e.text(name);
+    } else {
+      e.text(e.attr("data-default-name"));
+    }
+  });
+
+  $("#track-rename-pane .close").click(function() {
+    $("#track-rename-pane").fadeOut("fast");
+    $("#track-rename-pane form").submit();
+  });
+
+  $("#track-rename-pane form").submit(function() {
+    $("#track-rename-pane").fadeOut("fast");
+  });
+
+  $("#track-rename-link").click(function() {
+    $("#track-rename-pane").fadeIn("fast");
+    $("#track-rename-pane input[type='text']").focus();
+    return false;
+  });
+});
