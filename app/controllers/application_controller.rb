be299f4b14fde78cc0d6a47be302dd0bea907371
Thomas Cyron <thomas@thcyron.de>
2011-04-21 19:44:08 +0200

    Let the upfront server do the HTTP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :authenticate, :if => lambda {
-    Bikelog::Config.password and not Rails.env.development? and not Rails.env.test?
-  }
-
-  def authenticate
-    authenticate_or_request_with_http_basic "Bikelog" do |username, password|
-      password == Bikelog::Config.password
-    end
-  end
-  private :authenticate
 end
--- config/initializers/bikelog.rb
+++ config/initializers/bikelog.rb
@@ -1,13 +1,1 @@
 require "bikelog/core_ext"
-
-module Bikelog
-  class Config
-    class << self
-      attr_accessor :password
-    end
-  end
-end
-
-# If not nil, a HTTP authentication will be performed at each
-# request.  Only the entered password is checked against this value.
-Bikelog::Config.password = "password"
