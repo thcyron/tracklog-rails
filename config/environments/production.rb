cc29f90754a2b45707509eec74e41ff7b742f9a8
Thomas Cyron <thomas@thcyron.de>
2011-09-04 23:19:28 +0200

    Precompile JS files in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Bikelog::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(auth.js dashboard.js logs.js tracks.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
