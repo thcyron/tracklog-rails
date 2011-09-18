ac5e6243034890becf5175acfe77a4616507a8ee
Thomas Cyron <thomas@thcyron.de>
2011-09-18 13:10:22 +0200

    Don't compress JS and CSS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,8 @@ Bikelog::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # Don't compress JavaScripts and CSS
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
