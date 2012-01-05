4df2744b41e4723394b5dc75be477d0b132d7e43
Thomas Cyron <thomas@thcyron.de>
2012-01-05 22:16:15 +0100

    Use new hash symbol syntax in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Bikelog::Application.routes.draw do
-  root :to => "dashboard#index", :as => :dashboard
+  root to: "dashboard#index", as: :dashboard
 
-  match "login" => "auth#login", :as => :login, :via => [:get, :post]
-  put "logout" => "auth#logout", :as => :logout
+  match "login" => "auth#login", as: :login, via: [:get, :post]
+  put "logout" => "auth#logout", as: :logout
 
-  get "profile" => "profile#index", :as => :profile
-  put "profile" => "profile#update", :as => :update_profile
+  get "profile" => "profile#index",  as: :profile
+  put "profile" => "profile#update", as: :update_profile
 
   get "dashboard/activity_plots_data" => "dashboard#activity_plots_data"
 
-  get "admin" => "admin#index", :as => :admin
+  get "admin" => "admin#index", as: :admin
 
   resources :logs do
     collection do
-      get "year/:year" => :index, :as => :year
+      get "year/:year" => :index, as: :year
     end
 
     member do
@@ -25,8 +25,8 @@ Bikelog::Application.routes.draw do
       resources :trackpoints
 
       member do
-        match :transfer, :via => [:get, :post]
-        put "split/:trackpoint_id" => :split, :as => :split
+        match :transfer, via: [:get, :post]
+        put "split/:trackpoint_id" => :split, as: :split
       end
     end
   end
