250c7fc956a87302be7aa6bbce8871b37722daa2
Thomas Cyron <thomas@thcyron.de>
2012-01-08 09:23:06 +0100

    Fix Markdown parsing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def markdown(text)
+    raw Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true).render(text)
+  end
 end
