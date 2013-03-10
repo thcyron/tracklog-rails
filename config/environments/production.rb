8d7ea8bf23b1703bc1ccb49205a5f5c0975ecca6
Thomas Cyron <thomas@thcyron.de>
2013-03-10 15:47:42 +0100

    Fix config.assets.precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Tracklog::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(auth.js dashboard.js logs.js tracks.js)
+  config.assets.precompile.unshift(*%w(auth.js dashboard.js logs.js tracks.js))
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
