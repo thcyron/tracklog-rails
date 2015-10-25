c8d2f6889fddee8344f5f77b7bfa6a9437bc516e
Matthias Grundmann <matthias@glasmail.de>
2015-10-25 20:05:31 +0100

    Update user controller to newer syntax

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,47 +1,43 @@
 class Admin::UsersController < AdminController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
 
-    if @user.create(user_params)
-      redirect_to admin_path, :notice => "The new user has been added."
+    if @user.save
+      redirect_to admin_path, notice: 'The new user has been added.'
     else
-      flash[:error] = "There was an error adding the user."
+      flash[:error] = 'There was an error adding the user.'
       render :new
     end
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
-    if @user.update_attributes(user_params)
-      redirect_to admin_path, :notice => "User has been updated."
+    if @user.update(user_params)
+      redirect_to admin_path, notice: 'User has been updated..'
     else
-      flash[:error] = "There was an error updating the user."
+      flash[:error] = 'There was an error updating the user.'
       render :edit
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
-
-    if @user.destroy
-      flash[:notice] = "The user has been deleted."
-    else
-      flash[:error] = "There was an error deleting the user."
-    end
-
-    redirect_to admin_path
+    @user.destroy
+    redirect_to admin_path, notice: 'The user has been deleted.'
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :name, :password, :is_admin, :distance_units)
